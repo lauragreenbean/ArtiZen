4e35525650e4c98d9564ab513d9a7990460204e8
lauragreenbean <lmgreen@usc.edu>
2023-03-30 18:49:50 -0700

    installing p5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import Home from './pages/Home';
+import p5 from 'p5';
+
 import P5Sketch from './components/p5';
 
 function App() {
   return (
    <div>
-    <P5Sketch/>
+    <Home/>
    </div>
   );
 }
