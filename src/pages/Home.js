5842a98e2452b0bae048599846eb2125b993059e
lauragreenbean <lmgreen@usc.edu>
2023-03-30 18:34:04 -0700

    Adding home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-function Home() {
-  return (
-    <div>
+class Home extends React.Component {
+  render() {
+    return (
+
       <h1>Home</h1>
-    </div>
-  );
+
+    );
+  }
 }
+
+export default Home;
