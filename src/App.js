c4ce2b8d6f23aac360d440a3f30ea824ca5eebea
lauragreenbean <lmgreen@usc.edu>
2023-03-30 19:01:12 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Home from './pages/Home';
 import p5 from 'p5';
 import Triangle from './components/triangle';
-import P5Sketch from './components/P5Sketch';
 
 function App() {
   return (
