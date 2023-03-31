0481388470dfb2fe8b11023bf096a45988fe688a
lauragreenbean <lmgreen@usc.edu>
2023-03-30 19:00:11 -0700

    triangle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Home from './pages/Home';
 import p5 from 'p5';
-
+import Triangle from './components/triangle';
 import P5Sketch from './components/P5Sketch';
 
 function App() {
   return (
    <div>
-    <P5Sketch/>
+    <p>This is a Triangle</p>
+    <Triangle/>
    </div>
   );
 }
