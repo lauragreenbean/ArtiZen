a4d083e8dbfbf56ac99fd0c0244d27448f6f6889
lauragreenbean <lmgreen@usc.edu>
2023-03-30 18:45:17 -0700

    adding p5 to front page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import P5Sketch from './components/p5';
 
 function App() {
   return (
    <div>
-    <Home/>
+    <P5Sketch/>
    </div>
   );
 }
