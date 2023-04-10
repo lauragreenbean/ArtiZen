c66b210148f5590aca424720ebea36ede1faebba
lauragreenbean <lmgreen@usc.edu>
2023-04-10 15:54:26 -0700

    changing served file from index.html to App.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+ReactDOM.render(<App />, document.getElementById('root'));
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 import 'bootstrap/dist/js/bootstrap.js';
+
 
 ReactDOM.render(
   <React.StrictMode>
